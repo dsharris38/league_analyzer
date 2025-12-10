9859265b38f590d27e77d31dcf9230342bf52f0b
dsharris38 <harris.dylan38@gmail.com>
2025-12-10 17:23:47 -0500

    Fix: Define CORS_ALLOWED_ORIGINS to prevent build error

--- web_dashboard/backend/config/settings.py
+++ web_dashboard/backend/config/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = []
 #     "http://localhost:5173",
 #     "http://127.0.0.1:5173",
 # ]
